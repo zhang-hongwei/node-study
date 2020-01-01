9bbadc91d9673a260822837483e49a41602f4edf
zhanghongwei <zhang123910@live.com>
2020-01-01 12:32:54 +0800

    1

--- server/mongodb/index.js
+++ server/mongodb/index.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+
 const { Catten } = require('./schema/cat');
+
 mongoose.connect('mongodb://localhost/test');
 
 const db = mongoose.connection;
@@ -9,12 +11,5 @@ db.on('error', err => {
 });
 
 db.once('open', () => {
-  // const cat1 = new Catten({ name: '测试2',age: 10 });
-  // cat1.save(() => {
-  //   console.log('保存成功');
-  // });
-  // console.log('连接成功');
   console.log('查询=>', Catten.find({ name: '测试2' }));
 });
-
-// exports.db = db;
